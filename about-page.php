db18ac948784a5aca506b8891024bb87777a4cda
Steve <stevelima935@gmail.com>
2026-09-03 16:54:55 +0200

    page about et test

--- about-page.php
+++ about-page.php
@@ -7,15 +7,8 @@ Template Name: About Page
 <?php get_header(); ?>
 
 <main>
-    <h1><?php the_title(); ?></h1>
-
-    <?php
-    while ( have_posts() ) :
-        the_title();
-        the_post();
-        the_content();
-    endwhile;
-    ?>
+    
+    <p>Popo</p>
 </main>
 
 <?php get_footer(); ?>
